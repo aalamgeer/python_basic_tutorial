38d248fe6eb9e69ad760f79171a035f1e648e7c5
aalam <ranaaalamgeer92@gmail.com>
2019-01-03 13:06:40 +0530

    Class variable and object variable

--- class2.py
+++ class2.py
@@ -1,10 +1,21 @@
 class Employee:
-	leaves = 10
+	leaves = 10  # class variable 
 	
-aalam = Employee()
-rohan = Employee()
+	def __init__(self, aname, alname, asalary): #self is object name, This constuctor call when object create of class
+		self.name = aname
+		self.lname = alname
+		self.salary = asalary
+		self.leaves = 20  # If this variable set no_leaves method pick this otherwise class variable using (leaves)
+		
+	def no_leaves(self):
+		return self.name + '=' + str(self.leaves)
+	
+aalam = Employee("Rohan","Singh",444)
+rohan = Employee("AAlam","Rana",555)
 
-Employee.leaves = 20
+print(aalam.no_leaves())
 
-print(rohan.leaves)
-print(aalam.leaves)
+#Employee.leaves = 20
+
+#print(rohan.leaves)
+#print(aalam.leaves)
