c3ab60923bfc5a03778133d45e36b5426a3cce19
aalam <ranaaalamgeer92@gmail.com>
2019-02-12 16:49:44 +0530

    Check palindrome string another ways

--- palindrome.py
+++ palindrome.py
@@ -4,3 +4,7 @@ if string == rvs:
 	print "The string in palindrome"
 else:
 	print "Not palindrome"
+	
+#--------------------------------------------------- another way below -----
+if string[::] == string[::-1]:
+	print "palindrome"
