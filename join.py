f480dc12d66eb1c9a97e49613f94a97efeeda5e4
aalam <ranaaalamgeer92@gmail.com>
2019-02-14 16:42:11 +0530

    Join method with list, tuple and dictionary in python

--- join.py
+++ join.py
@@ -1,7 +1,10 @@
 #----- Join method is used to join all elements of list or tuple or dict--
 mylist = ['Apple', 'Banana', 'Orange']
 mytup = ('Lion', 'Cat', 'Dog')
+myDict = {"name": "John", "country": "Norway"} # ----------- When dict via join then only key will join in string
 mystr = " ".join(mylist)
 mystr2 = " ".join(mytup)
+mystr3 = " ".join(myDict)
 print mystr
 print mystr2
+print mystr3
