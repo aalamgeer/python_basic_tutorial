a327a1c6672764249ff016eb00f8b133b1395d31
aalam <ranaaalamgeer92@gmail.com>
2019-02-14 16:35:50 +0530

    Join method with list tuple in python

--- join.py
+++ join.py
@@ -1,4 +1,7 @@
 #----- Join method is used to join all elements of list or tuple or dict--
 mylist = ['Apple', 'Banana', 'Orange']
+mytup = ('Lion', 'Cat', 'Dog')
 mystr = " ".join(mylist)
+mystr2 = " ".join(mytup)
 print mystr
+print mystr2
