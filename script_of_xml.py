aaebc1ccee574d555b8723e689211262fde9a65b
aalamgeer <ranaaalamgeer92@gmail.com>
2019-03-07 23:23:50 +0530

    Function for xml file parseing

--- script_of_xml.py
+++ script_of_xml.py
@@ -1,1 +1,15 @@
 import os
+import xml.etree.ElementTree as ET
+
+path = 'C:\python_program'
+file_find = 'test.xml'
+
+def change_xml(path,file_find):
+    for file in os.listdir(path):
+        if file.endswith(file_find):
+            xmlfile = os.path.join(path, file_find)
+            tree = ET.parse(xmlfile)
+            root = tree.getroot()
+            print(root)
+
+change_xml(path, file_find)
