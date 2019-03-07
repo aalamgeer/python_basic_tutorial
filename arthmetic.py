4025d2d5cf04892283dfba8c728c4a087e0f6ec2
aalamgeer <ranaaalamgeer92@gmail.com>
2019-03-07 22:38:01 +0530

    New files of deffrent programs

---
+++ arthmetic.py
@@ -0,0 +1,6 @@
+print("sum of 3+2 ", 3+2)
+print("product of 3*3", 3*3)
+print("divide of 10/3 ",10/3)
+print("modules of 10%3 ",10%3)
+print("square of 10**3 ",10**3)
+
---
+++ newline.py
@@ -0,0 +1,8 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> xyz = "hello sir\" how are u"
+>>> print(xyz)
+hello sir" how are u
+>>> print(xyz[5:9])
+ sir
+>>> 
