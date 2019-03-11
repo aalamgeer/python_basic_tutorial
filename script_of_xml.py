5be16431a4fedb37665e961bf5b5640c5668d0ac
aalamgeer <ranaaalamgeer92@gmail.com>
2019-03-11 22:37:17 +0530

    Xml file for testing find and replce attribute value

--- script_of_xml.py
+++ script_of_xml.py
@@ -10,9 +10,15 @@ def change_xml(path,file_find):
             xmlfile = os.path.join(path, file_find)
             tree = ET.parse(xmlfile)
             root = tree.getroot()
-            print(root)
-            print(ET.tostring(root, encoding="utf-8").decode('utf-8'))
-            for child in root.iter(file_find):
+            #print(root)
+            #print(ET.tostring(root, encoding="utf-8").decode('utf-8'))
+            for child in root.iter('movie'):
                 print(child.attrib)
+            b2tf = root.find("./genre/decade/movie[@title]")
+            print(type(b2tf))
+            b2tf.attrib["title"] = "Back To The Future Aalam Geer Rana"
+            print(b2tf.attrib)
+            tree.write(xmlfile)
+            tree =ET.parse(xmlfile)
 
 change_xml(path, file_find)
