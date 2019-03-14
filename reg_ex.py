b61a93aee924bae2257584a187dc8e1075dc5551
aalamgeer <ranaaalamgeer92@gmail.com>
2019-03-14 22:39:34 +0530

    Learing the reguler expression wild card . commit

--- reg_ex.py
+++ reg_ex.py
@@ -1,7 +1,9 @@
 import re
-pattern = "Cookie"
+pattern = r"Cookie"  # r is called raw string used for handle special chrater or backslashes
 sequence = "Cookie"
-if re.match(pattern, sequence):
+if re.match(pattern, sequence):  #Match function return a object if text match otherwise none
     print ("Match")
 else:
     print ("Not match")
+
+# Wild card character . dot is most used special charater (A period matches any single charater)
