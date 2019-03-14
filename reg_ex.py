3e1ea28ff1d5a173ed24d9ffc2d932cba522b630
aalamgeer <ranaaalamgeer92@gmail.com>
2019-03-14 23:09:46 +0530

    Learing the reguler expression wild card \w commit

--- reg_ex.py
+++ reg_ex.py
@@ -6,4 +6,16 @@ if re.match(pattern, sequence):  #Match function return a object if text match o
 else:
     print ("Not match")
 
-# Wild card character . dot is most used special charater (A period matches any single charater)
+# Wild card character . dot is most used special charater (A period matches any single charater except new line)
+
+print(re.search(r'C.ok.e', 'Cookie').group()) # output will be cookie
+
+# \w small w use to match any single charater single digit or underscore
+match = re.search(r'A\wlam\weer', 'AalamGeer').group()
+print (match)
+
+# \W capital w use to matches any charater not part of small \w
+match = re.search(r'C\Wke', 'C@ke').group()
+print(match)
+
+
