97104104353f7ced71908e1cd7e69b0e8242db5c
aalamgeer <ranaaalamgeer92@gmail.com>
2019-03-19 23:06:36 +0530

    os chadir and listdir reading tutorial version two

--- work with excelfile/excelfile.py
+++ work with excelfile/excelfile.py
@@ -1,1 +1,9 @@
 import os
+
+cwd = os.getcwd()  # get current working directory
+print(cwd)
+
+os.chdir('C:\python_program')  #change directory
+files_Direc = (os.listdir('.')) #get all files and directory from folder
+for i in files_Direc:
+    print (i)
