c30ac41601b5c2045fc7eccd143d97f94b74f4d8
aalamgeer <ranaaalamgeer92@gmail.com>
2019-04-01 23:07:26 +0530

    Prctise for numeric conversion

--- practise.py
+++ practise.py
@@ -27,7 +27,7 @@ print(number)
 
 #Take numeric and convert them alphabat.
 num = input("Phone ")
-dictionary = {"1":"One","2":"Two","3":"Three","4":"Four","5":"Five"}
+dictionary = {"1":"One", "2":"Two", "3":"Three", "4":"Four", "5":"Five"}
 output = ''
 for ch in num:
 	output += dictionary.get(ch,"!")+ " "
